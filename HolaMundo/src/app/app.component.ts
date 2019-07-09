3ce00151229b5aed66f468b033527564f696caa3
laura.pmedeiro <LPEREZ@tcpsi.es>
2019-07-09 14:40:44 +0200

    Conceptos básicos de angular y creación de un componente

---
+++ HolaMundo/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title = 'Laura WEBAPP Test';
+  public description = 'Aprendiendo a usar Angular'
+}
