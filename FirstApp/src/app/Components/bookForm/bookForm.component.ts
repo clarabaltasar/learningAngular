32e8a9902be9ecb2af58a6c0aae5e02b92b64d18
laura.pmedeiro <LPEREZ@tcpsi.es>
2019-07-11 12:27:23 +0200

    App with different components and submit formulary

---
+++ FirstApp/src/app/Components/bookForm/bookForm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookForm',
+  templateUrl: './bookForm.component.html',
+  styleUrls: ['./bookForm.component.css']
+})
+export class BookFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- FirstApp/src/app/app.module.ts
+++ FirstApp/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { BodyComponent } from './Components/body/body.component';
+import { FormsModule} from '@angular/forms';
+import { BookFormComponent } from './Components/bookForm/bookForm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    BookFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
